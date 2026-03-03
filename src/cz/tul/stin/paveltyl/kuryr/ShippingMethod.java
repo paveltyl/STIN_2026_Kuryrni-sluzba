d5c38048483b8a9a6c124e310d45cc90ce675f44
Pavel Tyl <pavel.tyl@gmail.com>
2026-03-03 23:38:33 +0100

    Add initial class and interface

---
+++ src/cz/tul/stin/paveltyl/kuryr/ShippingMethod.java
@@ -0,0 +1,8 @@
+package cz.tul.stin.paveltyl.kuryr;
+
+// Rozhraní definující způsob dopravy
+public interface ShippingMethod {                     // Společný typ pro všechny druhy dopravy
+
+    double calculateCost(double weight);              // Každá doprava musí umět spočítat cenu podle váhy
+
+}
